1760b3576ab0d8cd78d7340b013258a159611974
Dima2162 <dmitrypetrov2162@gmail.com>
2023-03-16 18:27:20 +0300

    add .json-file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Card from './components/Card';
 import Cart from './components/Cart';
 import Header from './components/Header';
 import Search from './components/Search';
+import sneakers from './server/sneakers.json'
 
 function App() {
 	return (
@@ -11,8 +12,10 @@ function App() {
 			<Header />
 			<div className="content p-40">
 				<Search />
-				<div className="d-flex">
-					<Card />
+				<div className="d-flex flex-wrap ">
+					{sneakers.map((obj) => (
+						<Card model={obj.model} price={obj.price} image={obj.image} />
+					))}
 				</div>
 			</div>
 		</div>
